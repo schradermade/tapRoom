935c3ca8078a2c0c77e315ba4240a025ab196f73
Nathan Schrader <nathanschrader@icloud.com>
2021-02-12 15:22:17 -0800

    refactor Keg comp. to accept props

--- tap-room/src/components/Keg.js
+++ tap-room/src/components/Keg.js
@@ -1,10 +1,12 @@
 import React from "react";
 
-function Keg()  {
+function Keg(props)  {
   const name = "Evasion";
   return (
     <React.Fragment>
-  
+      <h1>{props.name} - {props.brand}</h1>
+      <h3>{props.abv}</h3>
+      <p><em>{props.price}</em></p>
     </React.Fragment>
   );
 }
